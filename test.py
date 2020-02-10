5ef773aa7d40f5e3379a6d802b74f0f570ac0461
AnupChy <anupch4984@gmail.com>
2020-02-10 21:06:04 +0600

    first commit

---
+++ test.py
@@ -0,0 +1,20 @@
+# if else statement
+age = 25
+
+if(age >= 25):
+    print("you are an adult")
+
+else:
+    print("you are not an adult")
+
+# if we can need add more condition
+age = 20
+
+if(age <=13):
+    print("you are an kid")
+elif(age <=18):
+    print("you are an teen ager")
+else:
+     print("you are not an adult")
+
+# second part condition we cant check out it change age value every single time
---
+++ var and math.py
@@ -0,0 +1,11 @@
+# details of python math and variable concatenation
+text = "we are "
+math = 5+5
+result = str(math)
+print(text+result)
+
+# auto detect variable value
+x,y,z = (3,5,8)
+print(x)
+print(y)
+print(z)
